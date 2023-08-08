368349d565490b1b1f16c0e35f7ab5f7bc2683a1
anv-pot <138220119+anv-pot@users.noreply.github.com>
2023-08-08 22:09:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
+const registrationForm = document.getElementById("registration-form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const dobInput = document.getElementById("dob");
+const termsCheckbox = document.getElementById("terms");
+const messageElement = document.getElementById("registration-message");
+const tableBody = document.getElementById("entries");
+
+// Load entries from local storage on page load
+document.addEventListener("DOMContentLoaded", function() {
+  loadEntries();
+});
+
 registrationForm.addEventListener("submit", function(event) {
   event.preventDefault();
 
-  // ... (validation and input processing)
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  const dob = dobInput.value;
+  const terms = termsCheckbox.checked;
 
-  // Add the entry to the table
+  // Additional validations
+  const today = new Date();
+  const dobDate = new Date(dob);
+  const age = today.getFullYear() - dobDate.getFullYear();
+  
+  if (!emailIsValid(email)) {
+    messageElement.textContent = "Please enter a valid email address.";
+    return;
+  }
+
+  if (age < 18 || age > 55) {
+    messageElement.textContent = "Please enter a valid date of birth (age must be between 18 and 55).";
+    return;
+  }
+
+  // Add the entry to the table and save to local storage
   const entry = {
     name,
     email,
@@ -12,8 +45,6 @@ registrationForm.addEventListener("submit", function(event) {
     terms
   };
   addEntryToTable(entry);
-
-  // Save the entry to web storage
   saveEntryToStorage(entry);
 
   // Clear form fields
@@ -25,3 +56,35 @@ registrationForm.addEventListener("submit", function(event) {
 
   messageElement.textContent = "Registration successful!";
 });
+
+function addEntryToTable(entry) {
+  const newRow = tableBody.insertRow();
+  const nameCell = newRow.insertCell();
+  const emailCell = newRow.insertCell();
+  const passwordCell = newRow.insertCell();
+  const dobCell = newRow.insertCell();
+  const termsCell = newRow.insertCell();
+
+  nameCell.textContent = entry.name;
+  emailCell.textContent = entry.email;
+  passwordCell.textContent = entry.password;
+  dobCell.textContent = entry.dob;
+  termsCell.textContent = entry.terms ? "Yes" : "No";
+}
+
+function saveEntryToStorage(entry) {
+  const entries = JSON.parse(localStorage.getItem("entries")) || [];
+  entries.push(entry);
+  localStorage.setItem("entries", JSON.stringify(entries));
+}
+
+function loadEntries() {
+  const entries = JSON.parse(localStorage.getItem("entries")) || [];
+  for (const entry of entries) {
+    addEntryToTable(entry);
+  }
+}
+
+function emailIsValid(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}

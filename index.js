72482feabdca4a01f182946276ca99d8cf4fda11
anv-pot <138220119+anv-pot@users.noreply.github.com>
2023-08-08 21:34:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
-document.getElementById("registration-form").addEventListener("submit", function(event) {
+const registrationForm = document.getElementById("registration-form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const dobInput = document.getElementById("dob");
+const termsCheckbox = document.getElementById("terms");
+const messageElement = document.getElementById("registration-message");
+const tableBody = document.getElementById("entries");
+
+registrationForm.addEventListener("submit", function(event) {
   event.preventDefault();
 
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const dob = document.getElementById("dob").value;
-  const terms = document.getElementById("terms").checked;
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  const dob = dobInput.value;
+  const terms = termsCheckbox.checked;
 
-  // Perform validation and registration logic here
+  // Additional validations
+  const today = new Date();
+  const dobDate = new Date(dob);
+  const age = today.getFullYear() - dobDate.getFullYear();
+  
+  if (age < 18 || age > 55) {
+    messageElement.textContent = "Please enter a valid date of birth (age must be between 18 and 55).";
+    return;
+  }
 
-  const messageElement = document.getElementById("registration-message");
+  // Add the entry to the table
+  const newRow = tableBody.insertRow();
+  const nameCell = newRow.insertCell();
+  const emailCell = newRow.insertCell();
+  const passwordCell = newRow.insertCell();
+  const dobCell = newRow.insertCell();
+  const termsCell = newRow.insertCell();
+
+  nameCell.textContent = name;
+  emailCell.textContent = email;
+  passwordCell.textContent = password;
+  dobCell.textContent = dob;
+  termsCell.textContent = terms ? "Yes" : "No";
+
+  // Clear form fields
+  nameInput.value = "";
+  emailInput.value = "";
+  passwordInput.value = "";
+  dobInput.value = "";
+  termsCheckbox.checked = false;
+
   messageElement.textContent = "Registration successful!";
 });
+
